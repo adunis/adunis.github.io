3bc4e484be2c894287b66ba6495be4f7723a8af2
adunis <goblinslayer3@gmail.com>
2023-04-07 21:01:02 +0200

    change a lot of stuff

--- javascript.js
+++ javascript.js
@@ -1,27 +1,85 @@
 import mustache from "https://cdn.skypack.dev/mustache@4.2.0";
+import html2ca2nvas from "https://cdn.skypack.dev/html2canvas";
+import $ from 'https://cdn.skypack.dev/jquery'
 
-
-var data = await fetch("HoH_creatures.json");
+var data = await fetch("HoH_characters.json");
 var jsonData = await data.json();
 
-for (var card in jsonData) {
+var dataItems = await fetch("HoH_items.json");
+var jsonDataItems = await dataItems.json();
+
+var dataFeatures = await fetch("HoH_features.json");
+var jsonDataFeatures = await dataFeatures.json();
+
+renderCards(jsonData);
+
+function renderCards(jsonData){
+
+    document.querySelector("#card-grid").innerHTML = "";
+    document.querySelector("#previewImg").innerHTML = "";
+
+
+    for (var card in jsonData) {
 
 // Use an if statement to check the type value and render the appropriate card
-    if (jsonData[card].type === "character") {
-        var cardTemplate = document.querySelector("#character-card-template").innerHTML;
-    } else if (jsonData[card].type.includes("item")) {
-        var cardTemplate = document.querySelector("#item-card-template").innerHTML;
-    } else if (jsonData[card].type === "feature") {
-        var cardTemplate = document.querySelector("#feature-card-template").innerHTML;
-    } else if (jsonData[card].type === "npc") { //npc with unique names and personalities, it can be 'linked' to a unnamed_actor card.
-        var cardTemplate = document.querySelector("#npc-card-template").innerHTML;
-    } else if (jsonData[card].type.includes("creature")) { //generic monsters,enemies and animals  like 'bandit' or 'horse' or 'red dragon'
-        var cardTemplate = document.querySelector("#creature-card-template").innerHTML;
-    } else if (jsonData[card].type === "rule") {
-        var cardTemplate = document.querySelector("#rule-card-template").innerHTML;
-    }
+  var cardTemplate = document.querySelector("#card-template").innerHTML;
 
 // Use a template engine like Mustache.js to render the JSON data into the card template
-    var renderedCard = mustache.render(cardTemplate, jsonData[card]);
-    document.querySelector("#card-grid").innerHTML += renderedCard;
+        var renderedCard = mustache.render(cardTemplate, jsonData[card]);
+        document.querySelector("#card-grid").innerHTML += renderedCard;
+    }
 }
+
+document.getElementById("btn-Convert-Html2Image").addEventListener("click", function() {
+
+var cards =  document.getElementsByClassName("card");
+    document.getElementById("previewImg").innerHTML += "<p><h3>Generated JPGs:</h3></p>";
+    document.getElementById("previewImg").innerHTML += "<p><button>Download All</button></p>";
+    burgerMenu.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+
+    for (var card in cards) {
+        const name = document.getElementsByClassName("title")[0].innerHTML;
+        html2canvas(
+            cards[card], { allowTaint: true, logging:true, taintTest: false}).then(function (canvas) {
+            var anchorTag = document.createElement("a");
+            document.body.appendChild(anchorTag);
+            document.getElementById("previewImg").appendChild(canvas);
+            anchorTag.download = name + ".jpg";
+            anchorTag.href = canvas.toDataURL();
+            anchorTag.target = '_blank';
+            anchorTag.click();
+        });
+    }
+});
+
+
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+    burgerMenu.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
+document.getElementById("load-features").addEventListener("click", async function () {
+        var dataFeatures = await fetch("HoH_features.json");
+        var jsonDataFeatures = await dataFeatures.json();
+        renderCards(jsonDataFeatures);
+        burgerMenu.classList.toggle("close");
+        overlay.classList.toggle("overlay");
+    });
+
+document.getElementById("load-characters").addEventListener("click", async function () {
+    var dataFeatures = await fetch("HoH_characters.json");
+    var jsonDataFeatures = await dataFeatures.json();
+    renderCards(jsonDataFeatures);
+    burgerMenu.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
+
+document.getElementById("load-items").addEventListener("click", async function () {
+    var dataFeatures = await fetch("HoH_items.json");
+    var jsonDataFeatures = await dataFeatures.json();
+    renderCards(jsonDataFeatures);
+    burgerMenu.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+});
