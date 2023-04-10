d3c83cfeb3c50559e0e63abbf19689405f876691
adunis <goblinslayer3@gmail.com>
2023-04-10 18:38:34 +0200

    added Loot Boosters Packs

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 import mustache from "https://cdn.skypack.dev/mustache@4.2.0";
 import html2canvas from "https://cdn.skypack.dev/html2canvas";
+import CryptoJS from 'https://cdn.skypack.dev/crypto-js';
 
 let selectedCards = 0;
 let requiredCrystals = {};
@@ -91,12 +92,33 @@ document.querySelector("#save-deck-button").addEventListener("click", () => {
     URL.revokeObjectURL(url);
 });
 
+document.querySelector("#save-deck-button-encrypted").addEventListener("click", () => {
+    const selectedCardsList = document.querySelector("#selected-cards-list");
+    const selectedCards = selectedCardsList.querySelectorAll("li");
+    const deckData = [];
+
+    for (const selectedCard of selectedCards) {
+        const cardData = JSON.parse(selectedCard.getAttribute("data-json"));
+        deckData.push(cardData);
+    }
+
+    const deckJson = JSON.stringify(deckData);
+    const encryptedJson = encrypt(deckJson); // encrypt the JSON data
+    const blob = new Blob([encryptedJson], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = "encrypted_booster.hoh"; // change the file extension to indicate that it is encrypted
+    a.href = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+});
+
 document.getElementById("btn-Convert-Html2Image").addEventListener("click", async function () {
     const cards = document.getElementsByClassName("card");
-    document.getElementById("pagination").innerHTML += "<p><h3>Generated JPGs:</h3></p>";
-    document.getElementById("pagination").innerHTML += "<p><button>Download All</button></p>";
-    burgerMenu.classList.toggle("close");
-    overlay.classList.toggle("overlay");
+    document.getElementById("previewImg").innerHTML += "<p><h3>Generated JPGs:</h3></p>";
+    //document.getElementById("pagination").innerHTML += "<p><button>Download All</button></p>";
 
     for (const card of cards) {
         const name = card.querySelector(".title").textContent;
@@ -141,7 +163,11 @@ function saveSelectedCards(cardElement) {
     const selectedCards = selectedCardsList.querySelectorAll("li").length;
 
     const saveDeckButton = document.querySelector("#save-deck-button");
+    const saveDeckButtonEncrypted = document.querySelector("#save-deck-button-encrypted");
+
     saveDeckButton.style.display = selectedCards > 0 ? "block" : "none";
+    saveDeckButtonEncrypted.style.display = selectedCards > 0 ? "block" : "none";
+
 
     if (selectedCards === 0) {
         requiredCrystals = {};
@@ -177,6 +203,12 @@ function saveSelectedCards(cardElement) {
     }
 
     selectedCount.textContent = selectedCards;
+}
+
+function encrypt(jsonData) {
+    const key = "my-secret-key"; // replace with your own secret key
+    const encJson = CryptoJS.AES.encrypt(jsonData, key).toString();
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encJson));
 }
 
 const burgerMenu = document.getElementById('burger-menu');
@@ -357,14 +389,11 @@ const generateBoosterPack = async () => {
     const response = await fetch("HoH_all.json");
     const data = await response.json();
 
-    const characters = data.filter(card => card.type === "character");
-    const features = data.filter(card => card.type === "feature");
-    const trainings = data.filter(card => card.type === "training");
-    const items = data.filter(card => card.type === "item" && card.type !== "magic");
-    const backgrounds = data.filter(card => card.type === "background");
-    const abilities = data.filter(card => card.type === "ability");
-
-
+    const characters = data.filter(card => card.type.includes("character"));
+    const features = data.filter(card => card.type.includes("feature"));
+    const trainings = data.filter(card => card.type.includes("training"));
+    const backgrounds = data.filter(card => card.type.includes("background"));
+    const abilities = data.filter(card => card.type.includes("ability"));
 
     const getRandomCards = (cards, count) => {
         const randomCards = [];
@@ -383,13 +412,41 @@ const generateBoosterPack = async () => {
 
     const randomCards = [
         ...getRandomCards(characters, 1),
-        ...getRandomCards(features, 1),
+        ...getRandomCards(features, 3),
         ...getRandomCards(trainings, 1),
-        ...getRandomCards(items, 3),
         ...getRandomCards(backgrounds, 1),
         ...getRandomCards(abilities, 1)
+    ];
 
+    const json = JSON.stringify(randomCards);
+    return randomCards;
+};
 
+const generateLootBoosterPack = async () => {
+    const response = await fetch("HoH_all.json");
+    const data = await response.json();
+
+    const items = data.filter(card => card.type.includes("item") && (!card.type.includes("common") || !card.type.includes("uncommon") || !card.type.includes("rare")));
+    const magic = data.filter(card => card.type.includes("item") &&  (card.type.includes("common") || card.type.includes("uncommon") || card.type.includes("rare")));
+
+    const getRandomCards = (cards, count) => {
+        const randomCards = [];
+
+        while (randomCards.length < count) {
+            const randomIndex = Math.floor(Math.random() * cards.length);
+            const card = cards[randomIndex];
+
+            if (!randomCards.includes(card)) {
+                randomCards.push(card);
+            }
+        }
+
+        return randomCards;
+    };
+
+    const randomCards = [
+        ...getRandomCards(items, 5),
+        ...getRandomCards(magic, 3)
     ];
 
     const json = JSON.stringify(randomCards);
@@ -404,6 +461,17 @@ document.querySelector("#booster-pack").addEventListener("click", async () => {
     const selectedCount = document.querySelector("#selected-count");
     selectedCount.textContent = selectedCards;
 filterData("", 1, randomCards);
+    console.log(randomCards);
+});
+
+document.querySelector("#booster-loot-pack").addEventListener("click", async () => {
+    const randomCards = await generateLootBoosterPack();
+    selectedCardsList.innerHTML = "";
+    removeAllButton.style.display = "none";
+    const selectedCards = selectedCardsList.querySelectorAll("li").length;
+    const selectedCount = document.querySelector("#selected-count");
+    selectedCount.textContent = selectedCards;
+    filterData("", 1, randomCards);
     console.log(randomCards);
 });
 
@@ -428,28 +496,33 @@ openBoosterPackButton.addEventListener("click", () => {
     boosterPackFileInput.click();
 });
 
+function decrypt(encryptedJson) {
+    const key = "my-secret-key"; // replace with your own secret key
+    const decData = CryptoJS.enc.Base64.parse(encryptedJson).toString(CryptoJS.enc.Utf8);
+    const bytes = CryptoJS.AES.decrypt(decData, key).toString(CryptoJS.enc.Utf8);
+    return JSON.parse(bytes);
+}
+
 boosterPackFileInput.addEventListener("change", function() {
     const file = this.files[0];
     const reader = new FileReader();
     reader.onload = function(event) {
-        const fileData = event.target.result;
         try {
-            loadedData = JSON.parse(fileData);
-            filterData("", 1, loadedData).then(() => {
+            const fileData = event.target.result;
+            const decryptedJson = decrypt(fileData);
+            filterData("", 1, decryptedJson).then(() => {
                 // Hide all cards in grid
                 const gridCards = document.querySelectorAll(".card-container");
                 gridCards.forEach((card) => {
                     card.classList.add("flipped");
                     card.addEventListener('click', () => {
                         card.classList.remove('flipped');
-
                     });
                 });
             });
         } catch (error) {
-            console.error("Invalid JSON file");
+            console.error("Invalid or corrupted encrypted file");
         }
     };
     reader.readAsText(file);
 });
-
